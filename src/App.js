f4248175d4d72abd5b8302c6fc2365e794a407f7
Peter Gallacher <93193880+gallapet@users.noreply.github.com>
2021-11-26 14:42:36 +0000

    commit to reset app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import React from "react";
-// import Webcam from "react-webcam";
-
-// const WebcamComponent = () => <webcam />
 
 function App() {
-  return (
-    <div className="App">
+ return (
+   <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         
@@ -15,13 +12,11 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React hello
+          rel="noopener noreferrer">
         </a>
       </header>
-    </div>
-  );
+      </div>          
+ );
 }
 
 export default App;
